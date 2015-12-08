478a8ed89ff4164ee41b2959576d269c068ef29f
Morteza Parvini <m.parvini@outlook.com>
2015-12-08 23:19:43 +0330

    Add config file

---
+++ config/payment-gateway.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'default_provider' => '',
+
+    'providers' => [
+
+
+        'payline' => [
+            'api' => '',
+            'gateway_send_url' => '',
+            'gateway_verify_url' => '',
+            'formatted_payment_url' => "http://payline.ir/payment/gateway-%d"
+        ]
+
+
+
+    ]
+];
